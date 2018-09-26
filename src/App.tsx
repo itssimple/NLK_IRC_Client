86cbf0e7eb6b091a99f0ea4372d4cb5568c8b803
lice <lice@multinet.se>
2018-09-26 12:56:28 +0200

    Fixed something

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,115 @@
-import * as React from 'react';
+import * as React from "react";
 import * as IRC from "irc-framework";
 import Icon from "antd/lib/icon";
-import Layout from 'antd/lib/layout';
-import Button from 'antd/lib/button';
-import 'antd/dist/antd.min.css';
-
+import Layout from "antd/lib/layout";
+import Button from "antd/lib/button";
+import "antd/dist/antd.min.css";
+import { Mention } from "antd";
 
 const { getGlobal, Menu, MenuItem } = window.require("electron").remote;
 
 const { Sider, Content, Footer } = Layout;
 
 const AppContext = React.createContext({
-   client: null
+	client: null
 });
 
-type Props = { /* */ };
+type Props = {
+	/* */
+};
 type AppState = {
-    client: IRC.IrcClient,
-    ircsettings: { server: string, port: number, nick: string, channel: string },
-    channels: []
-}
+	client: IRC.IrcClient;
+	ircsettings: {
+		server: string;
+		port: number;
+		nick: string;
+		channel: string;
+	};
+	channels: [];
+};
 
 class App extends React.Component<Props, AppState> {
-    constructor(props : Props) {
-        super(props);
-        this.state = {
-            client: getGlobal('irc_client'),
-            ircsettings: getGlobal('irc_settings'),
-            channels: getGlobal('irc_channels')
-        };
-    };
-    connectToServer = () => {
-        const client = this.state.client;
-        const settings = this.state.ircsettings;
-        if(!client.connected) {
-            client.connect({
-               host: settings.server,
-                port: settings.port,
-                nick: settings.nick
-            });
+	constructor(props: Props) {
+		super(props);
+		this.state = {
+			client: getGlobal("irc_client"),
+			ircsettings: getGlobal("irc_settings"),
+			channels: getGlobal("irc_channels")
+		};
+	}
+	connectToServer = () => {
+		const client = this.state.client;
+		const settings = this.state.ircsettings;
+		if (!client.connected) {
+			client.connect({
+				host: settings.server,
+				port: settings.port,
+				nick: settings.nick
+			});
 
-            client.on('registered', () => {
-                var chan = client.channel(settings.channel);
-                chan.join();
-                chan.say('Hey! 🍿');
-            });
-        }
-    };
-    disconnect = () => {
-        const client = this.state.client;
-        if(client.connected) {
-           client.quit('Bye bye!');
-        }
-    };
-    logMenu = () => {
-
-      console.log(Menu.getApplicationMenu().items);
-    };
-  render() {
-    return (
-        <AppContext.Provider value={this.state}>
-            <Layout style={{minHeight: '100vh'}}>
-                <Layout>
-                    <Sider width={200} style={{background: '#cecece', minHeight: '100%', padding: '5px'}}>
-                        <Button onClick={this.connectToServer} block><Icon type="login" theme="outlined" /> Connect</Button><br />
-                        <Button onClick={this.disconnect} block><Icon type="logout" theme="outlined" /> Disconnect</Button><br />
-                        <Button onClick={this.logMenu} block>Pewpew</Button>
-                    </Sider>
-                    <Layout style={{minHeight: '100%'}}>
-                        <Content style={{padding: '5px'}}>
-                            I'm an IRC client, herp derp!
-                        </Content>
-                    </Layout>
-
-                </Layout>
-                <Footer style={{background: '#999999'}}>
-                    <Content>
-                    </Content>
-                </Footer>
-            </Layout>
-        </AppContext.Provider>
-    );
-  }
+			client.on("registered", () => {
+				var chan = client.channel(settings.channel);
+				chan.join();
+				chan.say("Hey! 🍿");
+			});
+		}
+	};
+	disconnect = () => {
+		const client = this.state.client;
+		if (client.connected) {
+			client.quit("Bye bye!");
+		}
+	};
+	logMenu = () => {
+		console.log(Menu.getApplicationMenu().items);
+	};
+	render() {
+		return (
+			<AppContext.Provider value={this.state}>
+				<Layout style={{ minHeight: "100vh" }}>
+					<Layout>
+						<Sider
+							width={200}
+							style={{
+								background: "#cecece",
+								minHeight: "100%",
+								padding: "5px"
+							}}
+						>
+							<Button onClick={this.connectToServer} block>
+								<Icon type="login" theme="outlined" /> Connect
+							</Button>
+							<br />
+							<Button onClick={this.disconnect} block>
+								<Icon type="logout" theme="outlined" />{" "}
+								Disconnect
+							</Button>
+							<br />
+							<Button onClick={this.logMenu} block>
+								Pewpew
+							</Button>
+						</Sider>
+						<Layout style={{ minHeight: "100%" }}>
+							<Content style={{ padding: "5px" }}>
+								I'm an IRC client, herp derp!
+							</Content>
+						</Layout>
+					</Layout>
+					<Footer style={{ background: "#999999" }}>
+						<Content>
+							<Mention />
+						</Content>
+					</Footer>
+				</Layout>
+			</AppContext.Provider>
+		);
+	}
 }
 
 export default App;
 
 declare global {
-    interface Window {
-        require: any;
-    }
+	interface Window {
+		require: any;
+	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
 module.exports = {
-    mode: 'development',
-    entry: "./src/index.tsx",
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/public/dist'
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"]
-    },
-    module: {
-        rules: [
-            { test: /\.[tj]sx?$/, use: { loader: 'awesome-typescript-loader' } },
-            { test: /\.css$/, use: ['style-loader', 'css-loader' ] },
-            { enforce: "pre", test: /\.js$/, "loader": "source-map-loader" }
-        ]
-    },
-    devtool: "source-map",
-    target: "electron-main"
+	mode: "development",
+	entry: "./src/index.tsx",
+	output: {
+		filename: "[name].js",
+		path: __dirname + "/public/dist"
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".jsx"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "awesome-typescript-loader",
+				options: {
+					transpileOnly: true,
+					useBabel: true,
+					useCache: true,
+					babelOptions: {
+						babelrc: true
+					}
+				}
+			},
+			{ test: /\.css$/, use: ["style-loader", "css-loader"] },
+			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+		]
+	},
+	devtool: "source-map",
+	target: "electron-main"
 };
